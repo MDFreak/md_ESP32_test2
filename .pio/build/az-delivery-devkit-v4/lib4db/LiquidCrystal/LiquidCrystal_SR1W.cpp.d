59a2610454d25291236ead6e267b767c6b3fb131
MDFreak_ESP32 <post@martin-dorfner.at>
2023-05-13 22:01:28 +0200

    first commit

---
+++ .pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/LiquidCrystal_SR1W.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/LiquidCrystal_SR1W.cpp.o: \
+ .pio/libdeps/az-delivery-devkit-v4/LiquidCrystal/LiquidCrystal_SR1W.cpp
---
+++ .pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/SI2CIO.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/SI2CIO.cpp.o: \
+ .pio/libdeps/az-delivery-devkit-v4/LiquidCrystal/SI2CIO.cpp
